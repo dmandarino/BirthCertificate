e5d7cccd5743a63c90631c5969ad3b75fb19d1f6
dmandarino <Douglas Mandarino>
2019-10-19 23:51:13 -0300

    adding tests and creating first page

--- contracts/Notary.sol
+++ contracts/Notary.sol
@@ -1,16 +1,28 @@
 pragma solidity 0.5.8;
 pragma experimental ABIEncoderV2;
-import './BirthCertificate.sol';
 
-contract Notary is BirthCertificate {
+contract Notary {
 
+   struct BirthCertificate {
+        uint registrationNumber;
+        string name;
+        string city;
+        string gender;
+        string father;
+        string mother;
+        string paternalGrandfather;
+        string paternalGrandmother;
+        string maternalGrandfather;
+        string maternalGrandmother;
+        string witness;
+    }
     // Read/write Certificates
-    mapping(uint => BirthCertificate.Certificate) public certificates;
-    uint public certificateCount;
+    mapping(uint => BirthCertificate) public birthCertificates;
+    uint public birthCertificateCount;
 
     constructor() public {}
 
-    function createCertificate ( uint _registration,
+    function createBirthCertificate ( uint _registrationNumber,
                                 string memory _name,
                                 string memory _city,
                                 string memory _gender,
@@ -22,32 +34,55 @@ contract Notary is BirthCertificate {
                                 string memory _maternalGrandmother,
                                 string memory _witness ) public {
 
-        certificateCount ++;
-        certificates[certificateCount] = BirthCertificate.Certificate(certificateCount,
-                                                    _registration,
-                                                    _name,
-                                                    _city,
-                                                    _gender,
-                                                    _father,
-                                                    _mother,
-                                                    _paternalGrandfather,
-                                                    _paternalGrandmother,
-                                                    _maternalGrandfather,
-                                                    _maternalGrandmother,
-                                                    _witness);
+        birthCertificateCount ++;
+        BirthCertificate memory certificate;
+        certificate.registrationNumber = _registrationNumber;
+        certificate.name = _name;
+        certificate.city = _city;
+        certificate.gender = _gender;
+        certificate.father = _father;
+        certificate.mother = _mother;
+        certificate.paternalGrandfather = _paternalGrandfather;
+        certificate.paternalGrandmother = _paternalGrandmother;
+        certificate.maternalGrandfather = _maternalGrandfather;
+        certificate.maternalGrandmother = _maternalGrandmother;
+        certificate.witness = _witness;
+
+        validateBirthCertificate(certificate);
+
+        birthCertificates[birthCertificateCount] = certificate;
     }
 
-    function vote (uint _candidateId) public {
-        // // require that they haven't voted before
-        // require(!voters[msg.sender]);
+    // function vote (uint _candidateId) public {
+    //     // // require that they haven't voted before
+    //     // require(!voters[msg.sender]);
 
-        // // require a valid candidate
-        // require(_candidateId > 0 && _candidateId <= certificateCount);
+    //     // // require a valid candidate
+    //     // require(_candidateId > 0 && _candidateId <= certificateCount);
 
-        // // record that voter has voted
-        // voters[msg.sender] = true;
+    //     // // record that voter has voted
+    //     // voters[msg.sender] = true;
 
-        // // update candidate vote Count
-        // candidates[_candidateId].voteCount ++;
+    //     // // update candidate vote Count
+    //     // candidates[_candidateId].voteCount ++;
+    // }
+
+    function validateBirthCertificate(BirthCertificate memory certificate) private {
+        require(certificate.registrationNumber != 0, 'registrationNumber must not be empty');
+        require(bytes(certificate.name).length > 0, 'name must not be empty');
+        require(bytes(certificate.city).length > 0, 'city must not be empty');
+        bytes memory gender = bytes(certificate.gender);
+        require(keccak256(gender) == keccak256('M') || keccak256(gender) == keccak256('F'), 'gender must not be M or F');
+        require(bytes(certificate.father).length > 0, 'father must not be empty');
+        require(bytes(certificate.mother).length > 0, 'mother must not be empty');
+        require(bytes(certificate.paternalGrandfather).length > 0, 'paternalGrandfather must not be empty');
+        require(bytes(certificate.paternalGrandmother).length > 0, 'paternalGrandmother must not be empty');
+        require(bytes(certificate.maternalGrandfather).length > 0, 'maternalGrandfather must not be empty');
+        require(bytes(certificate.maternalGrandmother).length > 0, 'maternalGrandmother must not be empty');
+        require(bytes(certificate.witness).length > 0, 'witness must not be empty');
+
+        for ( uint i=0; i < birthCertificateCount; i++ ) {
+            require(birthCertificates[i].registrationNumber != certificate.registrationNumber, 'registrationNumber must be unique');
+        }
     }
 }
---
+++ test/notary.js
@@ -0,0 +1,120 @@
+var Notary = artifacts.require("./Notary.sol");
+
+contract("Notary", function(accounts) {
+  var notaryInstance;
+
+  it("initialize Notary", function() {
+    return Notary.deployed().then(function(instance) {
+      return instance.birthCertificateCount();
+    }).then(function(count) {
+      assert.equal(count, 0);
+    });
+  });
+
+  it("create a Birth Certificate", function() {
+    return Notary.deployed().then(function(instance) {
+      notaryInstance = instance;
+      notaryInstance.createBirthCertificate(99,
+                                          'Bruce Wayne',
+                                          'Gotham City',
+                                          'M',
+                                          'Thomas Wayne',
+                                          'Martha Wayne',
+                                          'Grandfather Wayne',
+                                          'Grandmother Wayne',
+                                          'Grandfather Mother',
+                                          'Grandmother Mother',
+                                          'Douglas Mandarino',
+                                          { from: accounts[1] });
+      return notaryInstance.birthCertificates(1);
+    }).then(function(certificate) {
+      assert.equal(certificate.registration, 99, "Create a Crtificate");
+      assert.equal(certificate.name, 'Bruce Wayne', "Create a Crtificate");
+      assert.equal(certificate.city, 'Gotham City', "Create a Crtificate");
+      assert.equal(certificate.gender, 'M', "Create a Crtificate");
+      assert.equal(certificate.father, 'Thomas Wayne', "Create a Crtificate");
+      assert.equal(certificate.mother, 'Martha Wayne', "Create a Crtificate");
+      assert.equal(certificate.paternalGrandfather, 'Grandfather Wayne', "Create a Crtificate");
+      assert.equal(certificate.paternalGrandmother, 'Grandmother Wayne', "Create a Crtificate");
+      assert.equal(certificate.maternalGrandfather, 'Grandfather Mother', "Create a Crtificate");
+      assert.equal(certificate.maternalGrandmother, 'Grandmother Mother', "Create a Crtificate");
+      assert.equal(certificate.witness, 'Douglas Mandarino', "Create a Crtificate");
+    });
+  });
+
+  it("throws an exception for empty field", function() {
+    return Notary.deployed().then(function(instance) {
+      notaryInstance = instance;
+      return notaryInstance.createBirthCertificate(99,
+                                            '',
+                                            'Gotham City',
+                                            'M',
+                                            'Thomas Wayne',
+                                            'Martha Wayne',
+                                            'Grandfather Wayne',
+                                            'Grandmother Wayne',
+                                            'Grandfather Mother',
+                                            'Grandmother Mother',
+                                            'Douglas Mandarino',
+                                            { from: accounts[1] });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('name must not be empty') >= 0, 'name must not be empty');
+    });
+  });
+
+  it("throws an exception for invalid gender", function() {
+    return Notary.deployed().then(function(instance) {
+      notaryInstance = instance;
+      return notaryInstance.createBirthCertificate(99,
+                                            'Bruce Wayne',
+                                            'Gotham City',
+                                            'T',
+                                            'Thomas Wayne',
+                                            'Martha Wayne',
+                                            'Grandfather Wayne',
+                                            'Grandmother Wayne',
+                                            'Grandfather Mother',
+                                            'Grandmother Mother',
+                                            'Douglas Mandarino',
+                                            { from: accounts[1] });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('gender must not be M or F') >= 0, 'gender must not be M or F');
+    });
+  });
+
+  it("throws an exception for duplicated certification number", function() {
+    return Notary.deployed().then(function(instance) {
+      notaryInstance = instance;
+      notaryInstance.createBirthCertificate(99,
+                                          'Bruce Wayne',
+                                          'Gotham City',
+                                          'M',
+                                          'Thomas Wayne',
+                                          'Martha Wayne',
+                                          'Grandfather Wayne',
+                                          'Grandmother Wayne',
+                                          'Grandfather Mother',
+                                          'Grandmother Mother',
+                                          'Douglas Mandarino',
+                                          { from: accounts[1] });
+      return notaryInstance.birthCertificates(1);
+    }).then(function(certificate) {
+        assert.equal(certificate.registration, 99, "Create a Crtificate");
+        notaryInstance.createBirthCertificate(99,
+                                          'Peter Parker',
+                                          'New York',
+                                          'M',
+                                          'Mr Parker',
+                                          'Mrs Parker',
+                                          'Grandfather Parker',
+                                          'Grandmother Parker',
+                                          'Grandfather Parker',
+                                          'Grandmother Parker',
+                                          'Douglas Mandarino',
+                                          { from: accounts[1] });
+    }).then(assert.fail).catch(function(error) {
+      // console.log(error.message.indexOf('registration must be unique'));
+      // assert(error.message.indexOf('revert') >= 0, 'registration must be unique');
+    });
+  });
+});
